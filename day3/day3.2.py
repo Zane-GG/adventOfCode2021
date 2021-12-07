ca7f4fe6aaf806aa8c6afb6ad8ef31abcdc1d5a1
Zane-GG <zane.gatti@outlook.com>
2021-12-07 12:29:13 +1100

    Before messing with count

--- day3/day3.2.py
+++ day3/day3.2.py
@@ -5,24 +5,27 @@ file = open("example.txt")
 o2 = ""
 c02 = ""
 data = []
+o2List = []
+co2List = []
 firstLine = True
 count = 0
 zeroC = 0
 oneC = 0
+hCount = 0
 
 # For each line in the file strip new lines
 for line in file:
     line = line.strip('\n')
+    o2List.append(line)
+    co2List.append(line)
     print(line)
 
     # For each element in a line
     for i in line:
 
-        # If its the first line then create a list for each element
         if firstLine:
             data.append([i])
 
-        # If its not the first line then append each elements to the each positional list
         else:
             data[count].append(i)
             count += 1
@@ -30,29 +33,54 @@ for line in file:
     firstLine = False
     count = 0
 
-# For each of the new vertical lists (gamma rate)
 for x in data:
-    print(x)
+    for i in x:
+        # print(x[count])
 
-    # For each element in the new lists increment the zeros and ones count
-    for a in x:
-        pass
+        if count < len(x) - 1:
+            print(i)
+            count += 1
 
-    # Reset the counts for next vertical list
-    oneC = 0
-    zeroC = 0
+            if i == "0":
+                zeroC += 1
 
-# For each of the new vertical lists (gamma rate)
-for f in data:
+            else:
+                oneC += 1
 
-    # For each element in the new lists increment the zeros and ones count
-    for e in f:
+        else:
+            print("Zeros =", zeroC)
+            print("Ones =", oneC)
+            print("END OF LIST")
 
-        pass
+            if zeroC > oneC:
+                for a in o2List:
 
-    # Reset the counts for next vertical list
-    oneC = 0
-    zeroC = 0
+                    if a[hCount] == "1":
+                        o2List.pop(0)
+                        # print(o2List)
+                # hCount += 1
+
+            elif zeroC < oneC:
+                for a in o2List:
+
+                    if a[hCount] == "0":
+                        o2List.pop(0)
+                        # print(o2List)
+                # hCount += 1
+
+            count = 0
+            zeroC = 0
+            oneC = 0
+            hCount += 1
+
+    print(o2List)
+
+
+
+
+print(data)
+print(o2List)
+print(co2List)
 
 
 
