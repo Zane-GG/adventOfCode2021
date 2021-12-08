e71e45b9dd9be294f1ebd5b5d4cb97826d39df35
Zane-GG <zane.gatti@outlook.com>
2021-12-08 12:57:30 +1100

    Changes made to day3.2

--- day3/day3.2.py
+++ day3/day3.2.py
@@ -5,19 +5,14 @@ file = open("example.txt")
 o2 = ""
 c02 = ""
 data = []
-o2List = []
-co2List = []
 firstLine = True
 count = 0
 zeroC = 0
 oneC = 0
-hCount = 0
 
 # For each line in the file strip new lines
 for line in file:
     line = line.strip('\n')
-    o2List.append(line)
-    co2List.append(line)
     print(line)
 
     # For each element in a line
@@ -33,54 +28,13 @@ for line in file:
     firstLine = False
     count = 0
 
-for x in data:
-    for i in x:
-        # print(x[count])
-
-        if count < len(x) - 1:
+    for x in data:
+        for i in x:
             print(i)
-            count += 1
-
-            if i == "0":
-                zeroC += 1
-
-            else:
-                oneC += 1
-
-        else:
-            print("Zeros =", zeroC)
-            print("Ones =", oneC)
-            print("END OF LIST")
-
-            if zeroC > oneC:
-                for a in o2List:
-
-                    if a[hCount] == "1":
-                        o2List.pop(0)
-                        # print(o2List)
-                # hCount += 1
-
-            elif zeroC < oneC:
-                for a in o2List:
-
-                    if a[hCount] == "0":
-                        o2List.pop(0)
-                        # print(o2List)
-                # hCount += 1
-
-            count = 0
-            zeroC = 0
-            oneC = 0
-            hCount += 1
-
-    print(o2List)
-
 
 
 
 print(data)
-print(o2List)
-print(co2List)
 
 
 
